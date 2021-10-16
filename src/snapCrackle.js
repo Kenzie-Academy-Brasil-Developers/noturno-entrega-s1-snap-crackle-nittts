a0b0753f532bb218e0a88e38424f3f22b04cb770
Nittts <w.baierle@gmail.com>
2021-10-16 10:23:26 -0300

    atividade realizada

--- src/snapCrackle.js
+++ src/snapCrackle.js
@@ -1,1 +1,20 @@
-// Seu código vai aqui
+function SnapCrackle(maxValue){
+    let resultado = "";
+    for (let index = 1; index <= maxValue; index++) {
+        if (index % 2 !== 0 && index % 5 === 0) {
+            resultado += "SnapCrackle, "
+            continue
+        }
+        if (index % 2 !== 0) {
+            resultado += "Snap, "
+            continue
+        }
+        if (index % 5 === 0) {
+            resultado += "Crackle, "
+            continue
+        }
+        resultado += index + ", "  
+}   
+    console.log(resultado)
+}
+SnapCrackle(12)
